84fc4c8631640c2e1fab42863717d98004381989
jparrker <102701917+jparrker@users.noreply.github.com>
2022-06-01 20:36:29 -0400

    fixed cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const PORT = 8000;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
 
 app.use(cors())
-
 const aliens = {
+  
   'humans':{
   'speciesName' : 'Humans',
   'homeworld': 'Earth',
@@ -68,7 +68,7 @@ app.get('/', (request, response)=>{
   response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:alienName', (request,response)=>{
+app.get('/api/:alienName', (request,response)=> {
   const aliensName = request.params.alienName.toLowerCase()
   if(aliens[aliensName]){
       response.json(aliens[aliensName])
